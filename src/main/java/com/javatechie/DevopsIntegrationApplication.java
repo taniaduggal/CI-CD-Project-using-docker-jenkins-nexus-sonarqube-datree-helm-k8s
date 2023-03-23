3cc18df7dcff7ad9debc1bc7a4128e1ab6d20f7f
Tania Duggal <103496926+taniaduggal@users.noreply.github.com>
2023-03-23 13:53:32 +0530

    Update DevopsIntegrationApplication.java

--- src/main/java/com/javatechie/DevopsIntegrationApplication.java
+++ src/main/java/com/javatechie/DevopsIntegrationApplication.java
@@ -11,7 +11,7 @@ public class DevopsIntegrationApplication {
 
 	@GetMapping
 	public String message(){
-		return "welcome to Mr. DevOps Youtube Channel";
+		return "Welcome to the tania's devops project";
 	}
 
 	public static void main(String[] args) {
